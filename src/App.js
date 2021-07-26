1ac1d0e433a5bef1ea407eba7dae186c9b385aeb
arundhathi <arundhathi@arundhathis-MacBook-Air.local>
2021-07-26 11:21:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 
+'react-router-dom';
+import Login from "/Users/arundhathi/Desktop/my-app/src/components/Login/Login.js";
+import Admin from "/Users/arundhathi/Desktop/my-app/src/components/Login/Admin.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Switch>
+        <Route exact path = "/" component = {Login}/>
+        <Route path = "/admin" component = {Admin}/>
+      </Switch>
+      
+    </Router>
+    </>
   );
 }
 
